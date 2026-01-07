6ebcae0e283e9d5f6658e6f9d85e71dc411a36b5
antonnagulin <nagulin03@mail.ru>
2026-01-07 20:20:22 +0300

    fix bags

--- project/books/serializers.py
+++ project/books/serializers.py
@@ -6,16 +6,16 @@ from .models import Book, UserBook
 class BooksListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        exclude = ['google_id', 'pages_count', 'categories']
+        #exclude = ['google_id', 'pages_count', 'categories']
+        fields = ("__all__")
+
 
     
 class BooksDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        # fields = ("__all__")
-        exclude = ['google_id', 'pages_count', 'categories']
-
-
+        fields = ("__all__")
+        #exclude = ['google_id', 'pages_count', 'categories']
 
 class UserListSerializer(serializers.ModelSerializer):
     class Meta:
